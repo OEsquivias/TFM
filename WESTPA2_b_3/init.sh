65c63e06661c87baaa27ef1507f6b450119cc2ca
OEquivias <uri_eskivias@hotmail.com>
2018-06-04 12:10:10 +0200

    Modification_WESTPA

--- WESTPA2_b_3/init.sh
+++ WESTPA2_b_3/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source env.sh
-ps aux | grep w_run | grep -v grep
-pkill -9 -f w_run
+
+
 
 SFX=.d$$
 mv traj_segs{,$SFX}
@@ -12,7 +12,7 @@ mkdir seg_logs traj_segs
 
 BSTATE_ARGS="--bstate start,1,bstates/frame.gro"
 
-#$WEST_ROOT/bin/w_init $BSTATE_ARGS $TSTATE_ARGS --segs-per-state 24 --work-manager=threads "$@"
+
 $WEST_ROOT/bin/w_init $BSTATE_ARGS $TSTATE_ARGS "$@"  &> init.log
 
 
