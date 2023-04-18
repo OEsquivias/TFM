b2098ff0c693655fe46f364e3a2fe1746ec8f285
Oriol Esquivias <32201176+OEsquivias@users.noreply.github.com>
2023-04-18 15:17:14 +0200

    Update trj_trace1_t3.sh

--- trj_trace1_t3.sh
+++ trj_trace1_t3.sh
@@ -10,7 +10,7 @@ for i in `tail -n +9 $1|awk '{print $1"-"$2}'`;do
     walk=`echo $i|sed 's/^[0-9]*//'|sed 's/-//'`
     p_walk=`printf "%06d" $walk`
 # Make the full path to the seg.xtc
-    TRJ_PATH=`printf "ramon@perdiu46:/s/ramon/WESTPA1_t_3/traj_segs/%06d/%06d" $iter $walk`
+    TRJ_PATH=`printf "user@address:/s/ramon/WESTPA1_t_3/traj_segs/%06d/%06d" $iter $walk`
 # Copy into the folder!
 #    printf "$TRJ_PATH\n" >> "trajectories.txt"
     scp $TRJ_PATH/seg_whole.xtc /home/ramon/PCNA/trace/all_xtc/${p_iter}_${p_walk}.xtc
